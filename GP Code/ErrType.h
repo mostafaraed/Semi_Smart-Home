fdf65a828abaad78a49aba8b6d90eb8f837371c1
Mostafa <mostafa.raed@yahoo.com>
2024-07-04 03:33:55 +0300

    Adding the libraries files

---
+++ GP Code/ErrType.h
@@ -0,0 +1,12 @@
+#ifndef ERRTYPE_H
+#define ERRTYPE_H
+
+#define NULL				((void*)0)
+
+#define OK					0u
+#define NOK					1u
+#define NULL_PTR_ERR		2u
+#define TIMEOUT_ERR			3u
+#define BUSY_ERR			4u
+
+#endif
